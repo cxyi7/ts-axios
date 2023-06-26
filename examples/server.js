c2481f005c1de1589319846887be15de6cd4a653
cxyi7 <1369276645@qq.com>
2023-06-26 22:47:03 +0800

    feat: 异常情况处理功能、接口拓展功能完成

--- examples/server.js
+++ examples/server.js
@@ -25,12 +25,14 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 
 const router = express.Router()
+// simple
 router.get('/simple/get',function (req,res) {
     res.json({
         msg: `hello world`
     })
 })
 
+// base
 router.get('/base/get',function (req,res) {
     res.json(req.query)
 })
@@ -48,6 +50,79 @@ router.post('/base/buffer', function (req,res) {
         let buf = Buffer.concat(msg)
         res.json(buf.toJSON())
    })
+})
+
+// error
+router.get('/error/get', function (req, res) {
+    if (Math.random() > 0.5) {
+        res.json({
+            msg: 'hello world'
+        })
+    } else {
+        res.status(500)
+        res.end()
+    }
+})
+
+router.get('/error/timeout', function (req, res) {
+    setTimeout(() => {
+        res.json({
+            msg: 'hello world'
+        })
+    }, 3000);
+})
+
+// extend
+router.get('/extend/get', function (req,res) {
+    res.json({
+        msg: 'it is get'
+    })
+})
+router.options('/extend/options',function (req,res) {
+    res.json({
+        msg: 'it is options'
+    })
+})
+
+router.delete('/extend/delete',function (req,res) {
+    res.json({
+        msg: 'it is delete'
+    })
+})
+
+router.head('/extend/head',function (req,res) {
+    res.json({
+        msg: 'it is head'
+    })
+})
+
+router.get('/extend/get',function (req,res) {
+    res.json({
+        msg: 'it is get'
+    })
+})
+
+router.post('/extend/post',function (req,res) {
+    res.json(req.body)
+})
+
+router.put('/extend/put',function (req,res) {
+    res.json(req.body)
+})
+
+router.patch('/extend/patch',function (req,res) {
+    res.json(req.body)
+})
+
+router.get('/extend/user', function (req,res) {
+    res.json({
+        code: 200,
+        message: 'ok',
+        result: {
+            name: 'hei',
+            age: 20
+        }
+    })
 })
 app.use(router)
 
---
+++ src/core/Axios.ts
@@ -0,0 +1,64 @@
+import { AxiosPromise, AxiosRequestConfig, Method } from "../type";
+import dispatchRequest from "./dispatchRequest";
+
+export default class Axios {
+  request(url: any, config?: any): AxiosPromise {
+    if (typeof url === 'string') {
+      if (!config) {
+        config = {}
+      }
+      config.url = url
+    } else {
+      config = url
+    }
+    return dispatchRequest(config)
+  }
+
+    get(url: string, config?: AxiosRequestConfig): AxiosPromise {
+        return this._requestMethodWidthoutData('get', url, config)
+    }
+
+    delete(url: string, config?: AxiosRequestConfig): AxiosPromise {
+        return this._requestMethodWidthoutData('delete', url, config)
+    }
+
+    head(url: string, config?: AxiosRequestConfig): AxiosPromise {
+        return this._requestMethodWidthoutData('head', url, config)
+    }
+
+    options(url: string, config?: AxiosRequestConfig): AxiosPromise {
+        return this._requestMethodWidthoutData('options', url, config)
+    }
+
+    post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+        return this._requestMethodWidthData('post', url,data, config)
+    }
+
+    put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+        return this._requestMethodWidthData('put', url, data, config)
+    }
+
+    patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+        return this._requestMethodWidthData('patch', url, data, config)
+    }
+    
+
+    _requestMethodWidthoutData(method: Method, url: string, config?: AxiosRequestConfig) {
+        return this.request(
+            Object.assign(config || {}, {
+                method,
+                url
+            })
+        )
+    }
+
+    _requestMethodWidthData(method: Method, url: string, data?: any, config?: AxiosRequestConfig) {
+        return this.request(
+          Object.assign(config || {}, {
+            method,
+            url,
+            data
+          })
+        )
+    }
+}
